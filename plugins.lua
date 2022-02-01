fbc9aace7a2753abb337cbc908a6a460a8b97dff
Adam Weinberger <adamw@adamw.org>
2022-02-01 03:36:21 -0700

    Add pretty-fold.nvim for fold previews

--- plugins.lua
+++ plugins.lua
@@ -34,6 +34,12 @@ require('packer').startup({function(use)
         vim.g.rsi_no_meta = true
     end}
 
+    -- Fold preview
+    use{'anuvyklack/pretty-fold.nvim', config = function()
+        require('pretty-fold').setup()
+        require('pretty-fold.preview').setup()
+    end}
+
     -- Fuzzy Finder
     use {'nvim-telescope/telescope.nvim',
         requires = {'nvim-lua/plenary.nvim'}
