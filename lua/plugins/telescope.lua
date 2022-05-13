6a1f351e1e74297437ed2d8fc20e1c56d2dbe498
Adam Weinberger <adamw@adamw.org>
2022-05-12 18:59:22 -0600

    Trim leading ./

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,9 @@ require('telescope').setup {
     defaults = {
         file_ignore_patterns = {'node_modules'},
         layout_strategy = 'flex',
+        path_display = function(_, path)  -- remove leading ./
+            return path:gsub('^./', '')
+        end,
     }
 }
 
