5390861718d30db6bbabec213aaeb7331a49f3e4
Adam Weinberger <adamw@adamw.org>
2022-05-14 16:01:27 -0600

    Set some more sensible defaults

--- lua/general.lua
+++ lua/general.lua
@@ -15,7 +15,8 @@ vim.opt.scrolljump  = 2        -- Min # of lines to scroll past screen
 vim.opt.scrolloff   = 2        -- Show this many lines around cursor
 vim.opt.showmatch   = true     -- Briefly flash matching bracket
 vim.opt.sidescroll  = 2        -- Min # of columns to scroll horizontally
-vim.opt.updatetime  = 100      -- Low is good for async
+vim.opt.splitbelow  = true     -- Open horizontal splits below
+vim.opt.splitright  = true     -- Open vertical splits to the right
 vim.opt.virtualedit = 'block'  -- Able to select nonexistent chars in v-block mode
 vim.opt.visualbell  = true     -- Do not *beep*ing beep
 
@@ -23,6 +24,7 @@ vim.opt.visualbell  = true     -- Do not *beep*ing beep
 vim.opt.grepprg    = 'grep --line-number --with-filename $*'
 vim.opt.ignorecase = true   -- Ignore case in search patterns
 vim.opt.smartcase  = true   -- Override 'ignorecase' when pattern has upper-case characters
+vim.opt.wildignore = vim.opt.wildignore + {'*/node_modules/*', '*/.git/*'}
 
 -- Tabbing/indenting (overridden prn in filetypes.lua)
 vim.opt.expandtab   = true
@@ -53,4 +55,9 @@ vim.opt.foldopen:append('jump')
 -- netrw
 vim.g.netrw_banner    = 0
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]  -- Hide dotfiles by default
-vim.g.netrw_liststyle = 3  -- Tree-style listing
+vim.g.netrw_liststyle = 3      -- Tree-style listing
+
+-- Performance
+vim.opt.timeoutlen  = 250      -- Wait time for map sequences
+vim.opt.ttimeoutlen = 10       -- Wait time for keycode sequences
+vim.opt.updatetime  = 100      -- Low is good for async
