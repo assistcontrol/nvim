ec6db97158699c596e100f29c039b7513ae4d86b
Adam Weinberger <adamw@adamw.org>
2022-05-15 12:39:08 -0600

    Let ^U clear input

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,11 @@ require('telescope').setup {
     defaults = {
         file_ignore_patterns = {'node_modules'},
         layout_strategy = 'flex',
+        mappings = {
+            i = {
+                ['<C-u>'] = false
+            }
+        },
         path_display = function(_, path)  -- remove leading ./
             return path:gsub('^./', '')
         end,
