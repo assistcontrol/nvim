e5d7f330a806fddac545f6c49fdcca3b667c194a
Adam Weinberger <adamw@adamw.org>
2022-05-16 15:44:10 -0600

    GUI can't always handle C-i in normal mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,5 @@
 require('toggleterm').setup {
-    open_mapping     = [[<C-i>]],
+    open_mapping     = [[<C-;>]],
     insert_mapping   = false, -- Only fire in normal mode
     terminal_mapping = true,  -- Link to open_mapping
 
