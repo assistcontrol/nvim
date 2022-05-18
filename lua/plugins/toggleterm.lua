75a6afb12c34778e2da74cfa1bbbf6aff9ceccc1
Adam Weinberger <adamw@adamw.org>
2022-05-18 05:06:43 -0600

    Require a double-tap

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,5 @@
 require('toggleterm').setup {
-    open_mapping     = [[<C-x>]],
+    open_mapping     = [[<C-x><C-x>]],
     insert_mapping   = false, -- Only fire in normal mode
     terminal_mapping = true,  -- Link to open_mapping
 
