43b242fecde99a2d809f70381fb703cebce28894
Adam Weinberger <adamw@adamw.org>
2022-05-19 09:21:43 -0600

    Explicit autocmds are more explicit

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -9,24 +9,19 @@ vim.filetype.add {
     }
 }
 
-local autocmd = {
-    -- Four-space expansion
-    ['Filetype go,lua,perl'] = 'shiftwidth=4 softtabstop=4 tabstop=4',
 
-    -- Unique widths
-    ['Filetype make'] = 'noexpandtab tabstop=8 shiftwidth=8',
-    ['Filetype text'] = 'textwidth=74',
+-- Four-space expansion
+vim.cmd [[autocmd Filetype go,lua,perl setlocal shiftwidth=4 softtabstop=4 tabstop=4]]
 
-    -- Expand tabs for system conf files and port Makefiles
-    ['BufRead,BufNewFile */ports/*,/etc/*,/usr/local/etc/*'] = 'noexpandtab tabstop=8 shiftwidth=8',
+-- Unique widths
+vim.cmd [[autocmd Filetype make setlocal noexpandtab tabstop=8 shiftwidth=8]]
+vim.cmd [[autocmd Filetype text setlocal textwidth=74]]
 
-    -- Spell-check git commits
-    ['BufRead COMMIT_EDITMSG'] = 'spell'
-}
+-- Expand tabs for system conf files and port Makefiles
+vim.cmd [[autocmd BufRead,BufNewFile */ports/*,/etc/*,/usr/local/etc/* setlocal noexpandtab tabstop=8 shiftwidth=8]]
 
-for target, action in pairs(autocmd) do
-    vim.cmd(string.format('autocmd %s setlocal %s', target, action))
-end
+-- Spell-check git commits
+vim.cmd [[autocmd BufRead COMMIT_EDITMSG setlocal spell]]
 
 -- Jump to position at last close
 vim.cmd([[autocmd BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal g'\"" | endif]])
