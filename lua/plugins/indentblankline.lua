d70919c9e2676fd716ee54effca2295d340a0cea
Adam Weinberger <adamw@adamw.org>
2022-05-23 15:10:25 -0600

    Deactivate on packer

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -1,6 +1,6 @@
 require('indent_blankline').setup {
     buftype_exclude  = {'help'},
-    filetype_exclude = {'alpha', 'dashboard', 'help', 'starter', 'startify'},
+    filetype_exclude = {'alpha', 'dashboard', 'help', 'packer', 'starter', 'startify'},
     show_current_context       = true,
     show_current_context_start = true
 }
