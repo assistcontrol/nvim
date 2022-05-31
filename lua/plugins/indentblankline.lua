e17b5fdbedddaa14d66ade4a7f981a0ff6dac012
Adam Weinberger <adamw@adamw.org>
2022-05-31 16:29:05 -0600

    Don't underline block start

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -2,7 +2,5 @@ require('indent_blankline').setup {
     buftype_exclude  = {'help', 'nofile'},
     filetype_exclude = {'alpha', 'dashboard', 'help', 'packer', 'starter', 'startify'},
     show_current_context       = true,
-    show_current_context_start = true,
-    show_current_context_start_on_current_line = true,
     use_treesitter             = true
 }
