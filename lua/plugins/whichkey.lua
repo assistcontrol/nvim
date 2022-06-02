b4eacd2ef90875a931ee49a0eaeb2766115144a8
Adam Weinberger <adamw@adamw.org>
2022-06-02 05:43:21 -0600

    Make the which-key float more visible

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,7 +1,14 @@
 local wk = require('which-key')
 
 wk.setup {
-    show_help = false
+    show_help = false,
+    window = {
+        winblend = AW.ui.winblend
+    }
 }
 
 wk.register(AW.maps.leader, {mode = 'n', prefix = '<leader>'})
+
+AW.defer [[
+    hi link WhichKeyFloat CustomMedium
+]]
