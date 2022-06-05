35581aa849a504ac3850ffee8003b9d3a9d3b7e4
Adam Weinberger <adamw@adamw.org>
2022-06-05 16:50:09 -0600

    More minimalistic output

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,6 +2,11 @@ local wk = require('which-key')
 
 wk.setup {
     show_help = false,
+    icons = {
+        breadcrumb = '',  -- Shown in command line
+        group      = AW.icon('lsp'),
+        separator  = ''   -- Between key and description
+    },
     window = {
         winblend = AW.ui.winblend
     }
