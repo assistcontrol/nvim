013f200b9d67d117defe0b637b7ea4f2944b3be9
Adam Weinberger <adamw@adamw.org>
2022-07-27 10:16:43 -0600

    Hide line numbers on inactive windows

--- lua/aw/plugins/focus.lua
+++ lua/aw/plugins/focus.lua
@@ -2,6 +2,23 @@ require('focus').setup {
     excluded_filetypes = {'NvimTree', 'starter'},
 
     cursorline = true,
-    number = false,
+    number     = true,
     signcolumn = false
 }
+
+
+-- Focus shows numbers on everything, even when they've
+-- been previously disabled. Re-disable them on enter.
+local bare_filetypes = {
+    help     = true,
+    NvimTree = true,
+    starter  = true
+}
+
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+    callback = function()
+        if bare_filetypes[vim.o.filetype] then
+            vim.opt_local.number = false
+        end
+    end
+})
