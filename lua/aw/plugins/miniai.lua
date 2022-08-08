b4c0a3c4b0a52c29b0ac1a2474da7827204c68cc
Adam Weinberger <adamw@adamw.org>
2022-08-08 07:51:13 -0600

    Add a TS @block matcher

--- lua/aw/plugins/miniai.lua
+++ lua/aw/plugins/miniai.lua
@@ -2,6 +2,7 @@ local treesitter_gen = require('mini.ai').gen_spec.treesitter
 
 require('mini.ai').setup {
     custom_textobjects = {
-        f = treesitter_gen {a = '@function.outer', i = '@function.inner'}
+        B = treesitter_gen {a = '@block.outer',    i = '@block.inner'},
+        F = treesitter_gen {a = '@function.outer', i = '@function.inner'}
     }
 }
