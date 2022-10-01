331ae0740562bb3e409290bf3c6dcb57acc8846b
Adam Weinberger <adamw@adamw.org>
2022-09-30 21:16:53 -0600

    Fix filetype loading in nvim 0.8

--- lua/aw/filetypes.lua
+++ lua/aw/filetypes.lua
@@ -1,9 +1,5 @@
 local autocmd = AW.autocmd
 
--- Use filetype.lua instead of filetype.vim
-vim.g.do_filetype_lua    = 1
-vim.g.did_load_filetypes = 0
-
 vim.filetype.add {
     extension = {
         pm = 'perl',
