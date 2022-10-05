243de685ca288eac8860c7a3259820476ef898e7
Adam Weinberger <adamw@adamw.org>
2022-10-04 19:12:09 -0600

    Just tint all the things

--- lua/aw/plugins/tint.lua
+++ lua/aw/plugins/tint.lua
@@ -1,12 +1,5 @@
 if not AW.has('tint') then return end
 
 require('tint').setup {
-    ignorefunc = function(winid)
-        -- Should we ignore? Return false: tint, true: do not tint
-        local buf     = vim.api.nvim_win_get_buf(winid)
-        local buftype = vim.api.nvim_buf_get_option(buf, 'buftype')
-
-        -- Only tint when buftype is ''
-        return not buftype == ''
-    end
+    tint = -50
 }
