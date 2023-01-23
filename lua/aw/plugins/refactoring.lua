f985a6a7c6361ab8765fcbef4efb7b3ec7d8a549
Adam Weinberger <adamw@adamw.org>
2023-01-23 11:50:24 -0700

    Disable refactoring for root

--- lua/aw/plugins/refactoring.lua
+++ lua/aw/plugins/refactoring.lua
@@ -1,6 +1,7 @@
 return {
     'theprimeagen/refactoring.nvim',
     dependencies = {AW.deps.plenary, AW.deps.treesitter},
+    enabled = not AW.is_root(),
     ft = {'javascript', 'lua', 'go', 'ruby'},
     config = function()
         require('refactoring').setup {}
