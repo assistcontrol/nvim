ebc9e85a7c7c96483c3ffac3a85d21db26ce853b
Adam Weinberger <adamw@adamw.org>
2023-02-27 17:30:20 -0500

    Modernize but keep inactive

--- lua/aw/plugins/inactive/cmp.lua
+++ lua/aw/plugins/inactive/cmp.lua
@@ -1,8 +1,3 @@
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
@@ -10,94 +5,35 @@ return {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
-    'hrsh7th/vim-vsnip',
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
   },
-  config = function()
-    -- Set up nvim-cmp.
+  event = 'InsertEnter',
+  opts = function()
     local cmp = require('cmp')
-
-    cmp.setup({
+    return {
+      completion = {
+        completeopt = 'menu,menuone,noinsert'
+      },
       snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        end,
+          require('luasnip').lsp_expand(args.body)
+        end
       },
-      window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-      },
-      mapping = {
-        ["<C-n>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<C-p>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ['<C-c>'] = cmp.mapping.close()
-      },
-      -- mapping = cmp.mapping.preset.insert({
-      --   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      --   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      --   ['<C-Space>'] = cmp.mapping.complete(),
-      --   ['<C-e>'] = cmp.mapping.abort(),
-      --   ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      -- }),
+      mapping = cmp.mapping.preset.insert({
+        ['<C-n>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
+        ['<C-p>'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-g>'] = cmp.mapping.abort(),
+        ['<CR>']  = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' }, -- For luasnip users.
-      }, {
-        { name = 'buffer' },
+        { name = 'nvim-lsp'},
+        { name = 'luasnip'},
+        { name = 'buffer'},
+        { name = 'path'}
       })
-    })
-
-    -- Set configuration for specific filetype.
-    -- cmp.setup.filetype('gitcommit', {
-    --   sources = cmp.config.sources({
-    --     { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    --   }, {
-    --     { name = 'buffer' },
-    --   })
-    -- })
-
-    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline({ '/', '?' }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
-    })
-
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      })
-    })
-
-    -- Set up lspconfig.
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-    require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-      capabilities = capabilities
     }
-
-  end
+  end,
 }
