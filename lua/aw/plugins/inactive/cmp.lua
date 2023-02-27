2494779f0b382acbf16c7dee6d4172439dd2a0be
Adam Weinberger <adamw@adamw.org>
2023-02-26 23:38:00 -0500

    Update but keep inactive

--- lua/aw/plugins/inactive/cmp.lua
+++ lua/aw/plugins/inactive/cmp.lua
@@ -1,71 +1,103 @@
-if not AW.has('cmp') then return end
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
-local cmp       = require('cmp')
-local luasnip   = require('luasnip')
-local lspconfig = require('lspconfig')
-local nvim_lsp  = require('cmp_nvim_lsp')
+return {
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/vim-vsnip',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = function()
+    -- Set up nvim-cmp.
+    local cmp = require('cmp')
 
-cmp.setup {
-    completion = {
-        autocomplete = false,  -- Don't complete automatically
-        completeopt = 'menu,menuone,noinsert'
-    },
-    confirmation = {default_behavior = cmp.ConfirmBehavior.Replace},
-    formatting = {
-        format = function(entry, vim_item)
-            -- Name each source
-            vim_item.menu = ({
-                buffer     = '[Buffer]',
-                calc       = '[Calc]',
-                nvim_lsp   = '[LSP]',
-                nvim_lua   = '[Lua]',
-                path       = '[Path]',
-                spell      = '[Spell]',
-                treesitter = '[TS]'
-            })[entry.source.name]
-            return vim_item
-        end
-    },
-    mapping = {
-        ['<C-n>'] = function()
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip and luasnip.expand_or_jumpable() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-            else
-                cmp.complete()
-            end
-        end,
-        ['<C-p>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip and luasnip.expand_or_jumpable(-1) then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-            else
-                cmp.complete()
-            end
-        end,
-        ['<C-c>'] = cmp.mapping.close()
-    },
-    snippet = {
+    cmp.setup({
+      snippet = {
+        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end
-    },
-    sources = {
-        --{name = 'treesitter'},  -- Slow, and may not even produce suggestions
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lua'},
-        {name = 'luasnip'},
-        {name = 'path'},
-        {name = 'calc'},
-        {name = 'buffer'},
-        {name = 'spell'},
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+      },
+      mapping = {
+        ["<C-n>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<C-p>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ['<C-c>'] = cmp.mapping.close()
+      },
+      -- mapping = cmp.mapping.preset.insert({
+      --   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      --   ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      --   ['<C-Space>'] = cmp.mapping.complete(),
+      --   ['<C-e>'] = cmp.mapping.abort(),
+      --   ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      -- }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+      }, {
+        { name = 'buffer' },
+      })
+    })
+
+    -- Set configuration for specific filetype.
+    -- cmp.setup.filetype('gitcommit', {
+    --   sources = cmp.config.sources({
+    --     { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    --   }, {
+    --     { name = 'buffer' },
+    --   })
+    -- })
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      })
+    })
+
+    -- Set up lspconfig.
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+    require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+      capabilities = capabilities
     }
+
+  end
 }
-
--- Load language servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = nvim_lsp.update_capabilities(capabilities)
-
-lspconfig['gopls'].setup {capabilities = capabilities}
