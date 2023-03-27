f73e7193892b2bb3ee1408c7155fbf1ebb8a193e
Adam Weinberger <adamw@adamw.org>
2023-03-27 09:10:30 -0400

    Tweak invocation slightly

--- lua/aw/plugins/spider.lua
+++ lua/aw/plugins/spider.lua
@@ -2,14 +2,14 @@ return {
     'chrisgrieser/nvim-spider',
     event = {'BufReadPre', 'BufNewFile'},
     config = function()
-        local spider = require('spider')
+        local spider = require('spider').motion
         local map = function(motion, desc)
-            vim.keymap.set({'n', 'o', 'x'}, motion, function() spider.motion(motion) end, { desc = desc })
+            vim.keymap.set({'n', 'o', 'x'}, motion, function() spider(motion) end, {desc = desc})
         end
 
-        map('w',  'next word (Spider)')
-        map('e',  'end of word (Spider)')
-        map('b',  'prev word (Spider)')
-        map('ge', 'end of prev  word (Spider)')
+        map('w',  'next word (nvim-spider)')
+        map('e',  'end of word (nvim-spider)')
+        map('b',  'prev word (nvim-spider)')
+        map('ge', 'end of prev  word (nvim-spider)')
     end
 }
