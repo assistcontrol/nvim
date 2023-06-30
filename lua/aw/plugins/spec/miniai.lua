fdef67c98a5e43167f7ca973610d022e7acd48d6
Adam Weinberger <adamw@adamw.org>
2023-06-29 22:23:13 -0400

    mini.ai: Use cover_or_nearest strategy

--- lua/aw/plugins/spec/miniai.lua
+++ lua/aw/plugins/spec/miniai.lua
@@ -6,5 +6,6 @@ require('mini.ai').setup {
     custom_textobjects = {
         B = treesitter_gen {a = '@block.outer',    i = '@block.inner'},
         F = treesitter_gen {a = '@function.outer', i = '@function.inner'}
-    }
+    },
+    search_method = 'cover_or_nearest'
 }
