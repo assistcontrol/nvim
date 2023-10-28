98fc9347c2c1f8542366563f83b58a3d62667ee6
Adam Weinberger <adamw@adamw.org>
2023-10-28 10:00:44 -0400

    Inactivate treesitter-textobjects due to lack of use

--- lua/aw/plugins/spec/miniai.lua
+++ lua/aw/plugins/spec/miniai.lua
@@ -1,11 +1,5 @@
 if not AW.has('mini.ai') then return end
 
-local treesitter_gen = require('mini.ai').gen_spec.treesitter
-
 require('mini.ai').setup {
-    custom_textobjects = {
-        B = treesitter_gen {a = '@block.outer',    i = '@block.inner'},
-        F = treesitter_gen {a = '@function.outer', i = '@function.inner'}
-    },
     search_method = 'cover_or_nearest'
 }
