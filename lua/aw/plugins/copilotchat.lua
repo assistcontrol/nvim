908efeef8e1e55e67c573aef29402885ac1ab93b
Adam Weinberger <adamw@adamw.org>
2024-10-11 09:48:44 -0500

    CopilotChat

---
+++ lua/aw/plugins/copilotchat.lua
@@ -0,0 +1,9 @@
+return {
+    'copilotc-nvim/copilotchat.nvim',
+    branch = 'canary',
+    dependencies = {
+        { 'zbirenbaum/copilot.lua' },
+        { 'nvim-lua/plenary.nvim' },
+    },
+    opts = {},
+}
