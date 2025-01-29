ee472c9ea2a3fa51b3234d3745685fd2fae56897
Adam Weinberger <aweinberger@glpo.org>
2025-01-29 14:13:42 -0500

    TS: Add many more parsers

--- lua/aw/plugins/treesitter.lua
+++ lua/aw/plugins/treesitter.lua
@@ -1,22 +1,43 @@
 -- Tree-sitter
 local parsers = AW.is_root() and {} or {
     'bash',
-    'git_config',
-    'gitignore',
+    'comment',  -- NOTE: INFO: WARN: ERROR: etc
+    'css',
+    'diff',
+    'gitcommit',
     'go',
     'html',
     'hurl',
     'javascript',
+    'jsdoc',
     'json',
     'perl',
     'python',
-    'robots',
     'ruby',
     'rust',
     'scss',
     'tmux',
+    'yaml',
+
+    -- Infrequent
+    'awk',
+    'cpp',
+    'c_sharp',
+    'editorconfig',
+    'git_config',
+    'git_rebase',
+    'gitignore',
+    'gomod',
+    'gosum',
+    'luap',
+    'powershell',
+    'printf',
+    'regex',
+    'robots',
+    'sql',
+    'toml',
+    'typescript',
     'typst',
-    'yaml'
 }
 
 return {
