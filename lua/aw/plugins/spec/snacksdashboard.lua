016bbe2b855963cee5d50d95751a8a42358fba92
Adam Weinberger <aweinberger@glpo.org>
2025-02-26 11:18:59 -0500

    Dashboard: Use keys rather than manual recreation

--- lua/aw/plugins/spec/snacksdashboard.lua
+++ lua/aw/plugins/spec/snacksdashboard.lua
@@ -1,50 +1,61 @@
 if vim.g.vscode then return {enabled = false} end
 
-local H = {
-    date = function()
-        return os.date('%a %Y/%m/%d %H:%M')
-    end,
+local date = function()
+    return os.date('%a %Y/%m/%d %H:%M')
+end
 
-    item = function(key, desc, icon, action)
-        return { key = key, desc = desc, icon = AW.icon(icon), action = action, indent = 2 }
-    end,
+local item = function(key, desc, icon, action)
+    return { key = key, desc = desc, icon = AW.icon(icon), action = action, indent = 2 }
+end
 
-    version = function()
-        local version = vim.version()
-        return string.format('nvim v%d.%d.%d', version.major, version.minor, version.patch)
-    end,
-}
+local newSection = function()
+    return { title = '' }
+end
+
+local title = function(str)
+    return { title = str }
+end
+
+local version = function()
+    local version = vim.version()
+    return string.format('nvim v%d.%d.%d', version.major, version.minor, version.patch)
+end
+
 
 return {
     preset = {
-        header = H.version(),
+        header = version(),
+        keys = {
+            title('Common'),
+            item('i', 'New file', 'new', [[:enew]]),
+            item('q', 'Quit nvim', 'quit', [[:qall]]),
+
+            newSection(),
+            title('Update'),
+            item('U', 'Update plugins',     'plug', [[:lua AW.update_plugins()]]),
+            item('G', 'Update go binaries', 'go',   [[:GoUpdateBinaries]]),
+            item('T', 'Update parsers',     'tree', [[:TSUpdate]]),
+
+            newSection(),
+            title('Browse'),
+            item('b', 'Browse',    'folder',   [[:lua AW.filebrowser()]]),
+            item('x', 'tree',      'filetree', [[:lua Snacks.picker.explorer({layout = {preset = 'default', preview = true}})]]),
+            item('g', 'live grep', 'search',   [[:lua Snacks.picker.grep()]]),
+            item('r', 'recent',    'recent',   [[:lua Snacks.picker.recent()]]),
+            item('n', 'ninja',     'ninja',    [[:lua AW.filebrowser('~/build/ninja')]]),
+            item('d', 'dotfiles',  'dotfiles', [[:lua AW.filebrowser('~/build/dotfiles')]]),
+            item('v', 'vim',       'vim',      [[:lua AW.filebrowser('~/build/nvim/lua/aw')]]),
+        },
     },
 
     sections = {
-        { title = H.version(), padding = {2, 0}, align = 'right' },
+        { title = version(), padding = {2, 0}, align = 'right' },
 
-        { title = 'Common' },
-        H.item('i', 'New file', 'new', [[:enew]]),
-        H.item('q', 'Quit nvim', 'quit', [[:qall]]),
+        { section = 'keys' },
 
-        { title = '' },
-        { title = 'Update' },
-        H.item('U', 'Update plugins',     'plug', [[:lua AW.update_plugins()]]),
-        H.item('G', 'Update go binaries', 'go',   [[:GoUpdateBinaries]]),
-        H.item('T', 'Update parsers',     'tree', [[:TSUpdate]]),
+        { pane = 2, title = date(), padding = {2, 0}, align = 'left' },
 
-        { title = '' },
-        { title = 'Browse' },
-        H.item('b', AW.pwd(),    'folder',   [[:lua AW.filebrowser()]]),
-        H.item('x', 'tree',      'filetree', [[:lua Snacks.picker.explorer({layout = {preset = 'default', preview = true}})]]),
-        H.item('g', 'live grep', 'search',   [[:lua Snacks.picker.grep()]]),
-        H.item('r', 'recent',    'recent',   [[:lua Snacks.picker.recent()]]),
-        H.item('n', 'ninja',     'ninja',    [[:lua AW.filebrowser('~/build/ninja')]]),
-        H.item('d', 'dotfiles',  'dotfiles', [[:lua AW.filebrowser('~/build/dotfiles')]]),
-        H.item('v', 'vim',       'vim',      [[:lua AW.filebrowser('~/build/nvim/lua/aw')]]),
-
-        { pane = 2, title = H.date(), padding = {2, 0}, align = 'left' },
-        { pane = 2, section = 'projects', icon = AW.icon('recent'), title = 'Projects', indent = 2 },
+        { pane = 2, title = 'Projects', section = 'projects', icon = AW.icon('recent'), indent = 2 },
 
         { pane = 2, title = '', padding = 0 },
         { pane = 2, section = 'terminal', icon = AW.icon('git'), title = 'Git Status', indent = 2, ttl = 60,
