7a28f19f9c9601516c2c6cc5f4463467f84ab248
Adam Weinberger <adamw@adamw.org>
2025-04-16 10:42:16 -0400

    rainbow-delimiters: Drop submodules (finally)

--- lua/aw/plugins/rainbow-delimiters.lua
+++ lua/aw/plugins/rainbow-delimiters.lua
@@ -1,5 +1,6 @@
 return {
     'hiphish/rainbow-delimiters.nvim',
+    submodules = false,
     enabled = not AW.is_root() and not vim.g.vscode,
-    event = {'BufReadPost', 'BufNewFile'}
+    event = {'BufReadPost', 'BufNewFile'},
 }
