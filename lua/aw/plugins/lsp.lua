06f6f3f431a4fff05aed1d6c269bc738b1b6a3a7
Adam Weinberger <adamw@adamw.org>
2025-12-08 13:36:59 -0500

    lsp: Fix lua_ls initialization

--- lua/aw/plugins/lsp.lua
+++ lua/aw/plugins/lsp.lua
@@ -59,31 +59,41 @@ return {
         }
         vim.lsp.enable('gopls')
 
-        vim.lsp.config.luals = {
+        vim.lsp.config.lua_ls = {
             conf,
             on_init = function(client)
-                client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJit',
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if path ~= vim.fn.stdpath('config') and
+                      (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+                        return
+                    end
+                end
+
+                client.config.settings = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        version = 'LuaJit',
+                        path = {
+                            'lua/?.lua',
+                            'lua/?/init.lua',
                         },
-                        diagnostics = {
-                            globals = {'vim'},
+                    },
+                    diagnostics = {
+                        globals = {'vim'},
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
                         },
-                        workspace = {
-                            checkThirdParty = false,
-                            library = {
-                                vim.env.VIMRUNTIME,
-                            },
-                        },
-                    }
+                    },
                 })
 
                 client.notify('workspace/didChangeConfiguration', {settings = client.config.settings})
                 return true
-            end
+            end,
         }
-        vim.lsp.enable('luals')
+        vim.lsp.enable('lua_ls')
 
         vim.lsp.config.rust_analyzer = {
             conf
