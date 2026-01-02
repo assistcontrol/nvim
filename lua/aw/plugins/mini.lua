6613dd907753fa8f4ad4fad831c56db20d7305be
Adam Weinberger <adamw@adamw.org>
2026-01-02 09:07:15 -0500

    Disable mini.cmdline. It's just too intrusive.

--- lua/aw/plugins/mini.lua
+++ lua/aw/plugins/mini.lua
@@ -20,9 +20,6 @@ return {
                 -- Completion
                 require('mini.completion').setup {}
 
-                -- Command line
-                require('mini.cmdline').setup {}
-
                 -- Diff
                 require('aw/plugins/spec/minidiff')
 
