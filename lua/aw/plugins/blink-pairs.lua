4e67af491d1e7eaea42f6eed6bad3bbf0fcb4f54
Adam Weinberger <adamw@adamw.org>
2026-06-02 17:32:29 -0400

    blink.pairs: Roll back to older version on macOS to fix build

--- lua/aw/plugins/blink-pairs.lua
+++ lua/aw/plugins/blink-pairs.lua
@@ -1,23 +1,44 @@
+local opts = {
+    highlights = {
+        groups = {
+            'BlinkPairsCyan',
+            'BlinkPairsPurple',
+            'BlinkPairsGreen',
+            'BlinkPairsOrange',
+            'BlinkPairsBlue',
+            'BlinkPairsYellow',
+        },
+
+        matchparen = {
+            include_surrounding = true,
+        },
+    },
+}
+
+-- Just produces errors on macOS. Not sure why yet...
+if AW.os('macOS') then
+    return {
+        'saghen/blink.pairs',
+        dependencies = 'saghen/blink.lib',
+        version = '*',
+
+        enabled = not AW.is_root() and not vim.g.vscode,
+        event = {'BufReadPre', 'BufNewFile'},
+
+        opts = opts,
+    }
+end
+
 return {
     'saghen/blink.pairs',
     dependencies = 'saghen/blink.lib',
 
     enabled = not AW.is_root() and not vim.g.vscode,
-    build = function() require('blink.pairs').build():pwait(60000) end,
-    opts = {
-        highlights = {
-            groups = {
-                'BlinkPairsCyan',
-                'BlinkPairsPurple',
-                'BlinkPairsGreen',
-                'BlinkPairsOrange',
-                'BlinkPairsBlue',
-                'BlinkPairsYellow',
-            },
+    event = {'BufReadPre', 'BufNewFile'},
 
-            matchparen = {
-                include_surrounding = true,
-            },
-        },
-    },
+    build = function() require('blink.pairs').build():pwait(60000) end,
+    config = function()
+        local ok, pairs = pcall(require, 'blink.pairs')
+        if ok then pairs.setup(opts) end
+    end,
 }
